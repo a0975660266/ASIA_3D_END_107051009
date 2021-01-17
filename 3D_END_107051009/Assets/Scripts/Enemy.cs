f2035afa42b95140af640637ffc550593e96a5b2
a0975660266 <71679957+a0975660266@users.noreply.github.com>
2021-01-17 15:59:59 +0800

    敵人AI

--- 3D_END_107051009/Assets/Scripts/Enemy.cs
+++ 3D_END_107051009/Assets/Scripts/Enemy.cs
@@ -31,7 +31,7 @@ public class Enemy : MonoBehaviour
         ani = GetComponent<Animator>();
 
         // 尋找其他遊戲物件("物件名稱").變形元件
-        player = GameObject.Find("小明").transform;
+        player = GameObject.Find("Player").transform;
         // 代理器 的 速度 與 停止距離
         nav.speed = speed;
         nav.stoppingDistance = stopDistance;
